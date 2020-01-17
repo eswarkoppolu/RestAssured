6949defd03f73a2e39c92bf9692e86f6f45e10a7
Reshma <Reshma@HP>
2020-01-17 08:40:14 +0530

    Third commit

--- Restassured2/src/Exapmle/Example1.java
+++ Restassured2/src/Exapmle/Example1.java
@@ -3,7 +3,13 @@ package Exapmle;
 public class Example1 {
 	
 public static void main(String[]args){
-	System.out.println("First commit");
+	
+	String email = "eswar_koppolu@gmail.com";
+    String[] splittedmail = email.split("[@]");
+    for (int j = 0; j <= splittedmail.length - 1; j++)
+    {
+        System.out.println("splited emails----------" + splittedmail[j]);
+    }
 }
 
 }
