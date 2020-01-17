d48bee87db42e14e08a0f80f60a755c3fb57363d
Reshma <Reshma@HP>
2020-01-17 08:43:42 +0530

    Fourth Commit

---
+++ Restassured2/src/Exapmle/Example2.java
@@ -0,0 +1,20 @@
+package Exapmle;
+
+public class Example2 {
+	
+	public void sum(){
+		int a=6;
+		int b=10;
+		int c=a+b;
+System.out.println(c);
+}
+
+	
+	public static void main(String[]args){
+		
+		
+		Example2 s1=new Example2();
+		s1.sum();
+		
+	}
+}
